9a3a4613c98f6652917a5cdf6800d3427b1e7aad
thibaut1405 <thibaut.villeneuve@epsi.fr>
2019-02-22 17:26:59 +0000

    modify init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #/bin/bash
-
+sudo apt update
+sudo apt upgrade
 sudo apt install -y openssh-server
-sudo rm /etc/ssh/sshd.config
+sudo rm /etc/ssh/sshd_config
 cp ~/tpSauvegarde/sshd_config /etc/ssh/sshd_config
 sudo apt install -y sshpass
