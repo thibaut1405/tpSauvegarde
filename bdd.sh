f7bca1a8ffd4cda5e9d794374e36d995834ad5aa
thibaut1405 <thibaut.villeneuve@epsi.fr>
2019-02-22 09:28:49 +0000

    add fonctionality to mysqldump

--- bdd.sh
+++ bdd.sh
@@ -2,6 +2,9 @@
 if [ ! -d /home/BDD_BACKUP ];
 then
         mkdir /home/BDD_BACKUP
+	mkdir /home/BDD_BACKUP/DAYLI_BACKUP
+	mkdir /home/BDD_BACKUP/WEEKLY_BACKUP
+	mkdir /home/BDD_BACKUP/MONTHLY_BACKUP
 fi
 
 dbname=nextcloud
@@ -16,7 +19,26 @@ sshpass -p "root" ssh root@192.168.33.200 'sudo -u www-data /usr/bin/php /var/ww
 sshpass -p "root" ssh root@192.168.33.200 "mysqldump --single-transaction -u $dbuser -p$dbpassword $dbname > ~/$databasebackup"
 
 # copy datas from to backup server
-sshpass -p "root" rsync -avx root@192.168.33.200:~/$databasebackup /home/BDD_BACKUP/$databasebackup
+
+sshpass -p "root" rsync --backup --backup-dir=`date +%Y.%m.%d` -a -e ssh 192.168.33.200:~/$databasebackup /home/BDD_BACKUP/DAYLI_BACKUP/$databasebackup
+
+
+
+ if [ "$numberOfDay" == "1" ];
+        then
+		rsync --delete -avr /home/DAYLI_BACKUP/DAYLI_`date +%Y.%m.%d`/ /home/WEEKLY_BACKUP
+        fi
+
+        if [ -d /home/BDD_BACKUP/DAYLI_BACKUP/db_$thirtyDayAgo.sql ];
+        then
+                rm -r /home/BDD_BACKUP/DAYLI_BACKUP/$thirtyDayAgo
+        fi
+        if [ "$day" == "01" ];
+        then
+        	rm /home/MONTHLY_BACKUP/*
+                tar -cvjf /home/BDD_BACKUP/MONTHLY_BACKUP/monthly_`date +%Y.%m.%d`.tar.bz2 /home/BDD_BACKUP/DAYLI_BACKUP/`date +%Y.%m.%d`/
+        fi
+
 
 # put nextcloud back to normal mode
 sshpass -p "root" ssh root@192.168.33.200 'sudo -u www-data /usr/bin/php /var/www/html/nextcloud/occ maintenance:mode --off'
